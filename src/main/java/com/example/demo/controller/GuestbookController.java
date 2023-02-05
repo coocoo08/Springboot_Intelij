3477ed7a8b7ba4e73426b55c1722d8537d1f6dc8
coocoo08 <futurely08@naver.com>
2023-02-05 23:19:32 +0900

    컨트롤러와 화면에서의 목록 처리

--- src/main/java/com/example/demo/controller/GuestbookController.java
+++ src/main/java/com/example/demo/controller/GuestbookController.java
@@ -1,18 +1,33 @@
 package com.example.demo.controller;
 
+import com.example.demo.dto.PageRequestDTO;
+import com.example.demo.service.GuestbookService;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/guestbook")
 @Log4j2
+@RequiredArgsConstructor    // 자동 주입을 위한 Annotation
 public class GuestbookController {
 
-    @GetMapping({"/", "/list"})
-    public String list(){
-        log.info("list.................");
-        return "/guestbook/list";
+    private final GuestbookService service;
+
+    @GetMapping("/")
+    public String index(){
+
+        return "redirect:/guestbook/list";
+    }
+
+    @GetMapping("/list")
+    public void list(PageRequestDTO pageRequestDTO, Model model){
+
+        log.info("list............." + pageRequestDTO);
+
+        model.addAttribute("result", service.getList(pageRequestDTO));
     }
 }
