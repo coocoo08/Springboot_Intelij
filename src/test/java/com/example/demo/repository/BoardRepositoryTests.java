7bc523fb74717a6b475a8155cad5422e5cba7232
coocoo08 <futurely08@naver.com>
2023-02-08 20:30:08 +0900

    JQSL를 활용한 조회 화면, 목록 화면 Test 구현

--- src/test/java/com/example/demo/repository/BoardRepositoryTests.java
+++ src/test/java/com/example/demo/repository/BoardRepositoryTests.java
@@ -5,6 +5,10 @@ import com.example.demo.entity.Member;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.lang.reflect.Array;
@@ -61,5 +65,22 @@ public class BoardRepositoryTests {
         for (Object[] arr : result){
             System.out.println(Arrays.toString(arr));
         }
+    }
+
+    @Test
+    public void testWithReplyCount(){
+        Pageable pageable = PageRequest.of(0, 10, Sort.by("bno").descending());
+        Page<Object[]> result = boardRepository.getBoardWithReplyCount(pageable);
+        result.get().forEach(row -> {
+            Object[] arr = (Object[])row;
+            System.out.println(Arrays.toString(arr));
+        });
+    }
+
+    @Test
+    public void testRead3() {
+        Object result = boardRepository.getBoardByBno(100L);
+        Object[] arr = (Object[])result;
+        System.out.println(Arrays.toString(arr));
     }
 }
