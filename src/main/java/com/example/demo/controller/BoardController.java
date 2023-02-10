909e8a957ce79eb24539243998ff1bd344c73e67
coocoo08 <futurely08@naver.com>
2023-02-10 16:34:04 +0900

    boardcontroller register함수 추가, register.html 생성

--- src/main/java/com/example/demo/controller/BoardController.java
+++ src/main/java/com/example/demo/controller/BoardController.java
@@ -1,5 +1,6 @@
 package com.example.demo.controller;
 
+import com.example.demo.dto.BoardDTO;
 import com.example.demo.dto.PageRequestDTO;
 import com.example.demo.service.BoardService;
 import lombok.RequiredArgsConstructor;
@@ -7,7 +8,9 @@ import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 @RequestMapping("/board")
@@ -21,5 +24,20 @@ public class BoardController {
     public void list(PageRequestDTO pageRequestDTO, Model  model){
         log.info("list..................." + pageRequestDTO);
         model.addAttribute("result", boardService.getList(pageRequestDTO));
+    }
+
+    @GetMapping("/register")
+    public void register(){
+        log.info("register get......");
+    }
+
+    @PostMapping("/register")
+    public String registerPost(BoardDTO dto, RedirectAttributes redirectAttributes){
+        log.info("dto...." + dto);
+        // 새로 추가된 엔티티의 번호
+        Long bno = boardService.register(dto);
+        log.info("BNO : " + bno);
+        redirectAttributes.addFlashAttribute("msg", bno);
+        return "redirect:/board/list";
     }
 }
