b74d5675e3ff9853c12e30ac55c1a1c6f6de5b59
coocoo08 <futurely08@naver.com>
2023-02-11 12:09:40 +0900

    repository 확장 Tuple 객체

--- src/main/java/com/example/demo/repository/search/SearchBoardRepositoryImpl.java
+++ src/main/java/com/example/demo/repository/search/SearchBoardRepositoryImpl.java
@@ -2,7 +2,9 @@ package com.example.demo.repository.search;
 
 import com.example.demo.entity.Board;
 import com.example.demo.entity.QBoard;
+import com.example.demo.entity.QMember;
 import com.example.demo.entity.QReply;
+import com.querydsl.core.Tuple;
 import com.querydsl.jpa.JPQLQuery;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.data.jpa.repository.support.QuerydslRepositorySupport;
@@ -23,15 +25,22 @@ public class SearchBoardRepositoryImpl extends QuerydslRepositorySupport impleme
         log.info("search1......................................");
         QBoard board = QBoard.board;
         QReply reply = QReply.reply;
+        QMember member = QMember.member;
+
         JPQLQuery<Board> jpqlQuery = from(board);
-        jpqlQuery.select(board).where(board.bno.eq(1L));
+        jpqlQuery.leftJoin(member).on(board.writer.eq(member));
         jpqlQuery.leftJoin(reply).on(reply.board.eq(board));
 
+        JPQLQuery<Tuple> tuple = jpqlQuery.select(board, member.email, reply.count());
+        tuple.groupBy(board);
+
         log.info("------------------------");
-        log.info(jpqlQuery);
+        log.info(tuple);
         log.info("------------------------");
 
-        List<Board> result = jpqlQuery.fetch();
+        List<Tuple> result = tuple.fetch();
+
+        log.info(result);
 
         return null;
     }
