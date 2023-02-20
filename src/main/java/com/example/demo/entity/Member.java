c1895a7dc4ad90a482822b05ad3693bdb856058f
coocoo08 <futurely08@naver.com>
2023-02-20 23:51:36 +0900

    member entity 수정

--- src/main/java/com/example/demo/entity/Member.java
+++ src/main/java/com/example/demo/entity/Member.java
@@ -2,8 +2,7 @@ package com.example.demo.entity;
 
 import lombok.*;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity
 @Builder
@@ -11,10 +10,12 @@ import javax.persistence.Id;
 @NoArgsConstructor
 @Getter
 @ToString
+@Table(name = "m_member")
 public class Member extends BaseEntity{
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private String email;
     private String password;
-    private String name;
+    private String nickname;
 }
