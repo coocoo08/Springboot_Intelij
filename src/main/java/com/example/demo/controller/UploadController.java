b30ab8fce10750f4453aad262cf3418d0478c066
coocoo08 <futurely08@naver.com>
2023-02-25 00:56:36 +0900

    UploadController 업데이트(jpg파일 아닐 시 403에러 확인)

--- src/main/java/com/example/demo/controller/UploadController.java
+++ src/main/java/com/example/demo/controller/UploadController.java
@@ -1,8 +1,11 @@
 package com.example.demo.controller;
 
 
+import com.example.demo.dto.UploadResultDTO;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
@@ -13,6 +16,8 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
 
 @RestController
@@ -23,13 +28,15 @@ public class UploadController {
     private String uploadPath;
 
     @PostMapping("/uploadAjax")
-    public void uploadFile(MultipartFile[] uploadFiles){
-        for (MultipartFile uploadFile : uploadFiles){
+    public ResponseEntity<List<UploadResultDTO>> uploadFile(MultipartFile[] uploadFiles){
 
+        List<UploadResultDTO> resultDTOList = new ArrayList<>();
+
+        for (MultipartFile uploadFile : uploadFiles){
             // 이미지 파일만 업로드 가능
             if (uploadFile.getContentType().startsWith("image") == false){
                 log.warn("this file is not image type");
-                return;
+                return new ResponseEntity<>(HttpStatus.FORBIDDEN);
             }
             // 실제 파일 이름 IE나 Edge는 전체 경로가 들어오므로
             String originalName = uploadFile.getOriginalFilename();
@@ -48,11 +55,13 @@ public class UploadController {
             Path savaPath = Paths.get(saveName);
 
             try {
-                uploadFile.transferTo(savaPath);
+                uploadFile.transferTo(savaPath); // 실제 이미지 저장
+                resultDTOList.add(new UploadResultDTO(fileName, uuid, folderPath));
             } catch (IOException e){
                 e.printStackTrace();
             }
         }// end for
+        return new ResponseEntity<>(resultDTOList, HttpStatus.OK);
     }
 
     private String makeFolder(){
