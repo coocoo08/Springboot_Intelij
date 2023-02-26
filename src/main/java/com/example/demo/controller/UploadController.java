9a564c4f3a8c31d5febafdf5ac5c3ad40c5eec74
coocoo08 <futurely08@naver.com>
2023-02-26 22:09:50 +0900

    UploadController 업데이트 ResponseEntity 설정

--- src/main/java/com/example/demo/controller/UploadController.java
+++ src/main/java/com/example/demo/controller/UploadController.java
@@ -4,14 +4,19 @@ package com.example.demo.controller;
 import com.example.demo.dto.UploadResultDTO;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.util.FileCopyUtils;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.File;
 import java.io.IOException;
+import java.net.URLDecoder;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.time.LocalDateTime;
@@ -62,6 +67,32 @@ public class UploadController {
             }
         }// end for
         return new ResponseEntity<>(resultDTOList, HttpStatus.OK);
+    }
+
+    @GetMapping("/display")
+    public ResponseEntity<byte[]> getFile(String fileName){
+
+        ResponseEntity<byte[]> result = null;
+        try {
+            String srcFileName = URLDecoder.decode(fileName, "UTF-8");
+
+            log.info("fileName : " + srcFileName);
+
+            File file = new File(uploadPath + File.separator + srcFileName);
+
+            log.info("file : " + file);
+
+            HttpHeaders header = new HttpHeaders();
+
+            // MIME타입 처리
+            header.add("Content-Type", Files.probeContentType(file.toPath()));
+            // 파일 데이터 처리
+            result = new ResponseEntity<>(FileCopyUtils.copyToByteArray(file), header, HttpStatus.OK);
+        } catch (Exception e){
+            log.info(e.getMessage());
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+        return result;
     }
 
     private String makeFolder(){
