1370a7878e42043632f16708d4a150e8fcda8cc5
coocoo08 <futurely08@naver.com>
2023-03-11 00:04:13 +0900

    MovieService, MovieServiceImpl를 dtoToEntity()에서 반환한 객체들을 이용해서 save()

--- src/main/java/com/example/demo/service/MovieService.java
+++ src/main/java/com/example/demo/service/MovieService.java
@@ -1,7 +1,43 @@
 package com.example.demo.service;
 
 import com.example.demo.dto.MovieDTO;
+import com.example.demo.dto.MovieImageDTO;
+import com.example.demo.entity.Movie;
+import com.example.demo.entity.MovieImage;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 public interface MovieService {
     Long register(MovieDTO movieDTO);
+
+    default Map<String, Object> dtoEntity(MovieDTO movieDTO){ // Map타입으로 반환
+        Map<String, Object> entityMap = new HashMap<>();
+
+        Movie movie = Movie.builder()
+                .mno(movieDTO.getMno())
+                .title(movieDTO.getTitle())
+                .build();
+
+        entityMap.put("movie", movie);
+
+        List<MovieImageDTO> imageDTOList = movieDTO.getImageDTOList();
+
+        // MovieImageDTO 처리
+        if (imageDTOList != null && imageDTOList.size() > 0){
+            List<MovieImage> movieImageList = imageDTOList.stream().map(movieImageDTO -> {
+                MovieImage movieImage = MovieImage.builder()
+                        .path(movieImageDTO.getPath())
+                        .imgName(movieImageDTO.getImgName())
+                        .uuid(movieImageDTO.getUuid())
+                        .movie(movie)
+                        .build();
+                return movieImage;
+            }).collect(Collectors.toList());
+            entityMap.put("imgList", movieImageList);
+        }
+        return entityMap;
+    }
 }
--- src/main/java/com/example/demo/service/impl/MovieServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/MovieServiceImpl.java
@@ -1,21 +1,41 @@
 package com.example.demo.service.impl;
 
 import com.example.demo.dto.MovieDTO;
+import com.example.demo.entity.Movie;
+import com.example.demo.entity.MovieImage;
+import com.example.demo.repository.MovieImageRepository;
 import com.example.demo.repository.MovieRepository;
 import com.example.demo.service.MovieService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Map;
 
 @Service
 @Log4j2
 @RequiredArgsConstructor
 public class MovieServiceImpl implements MovieService {
 
-    private final MovieRepository movieRepository;
+    private final MovieRepository movieRepository; //final
 
+    private final MovieImageRepository imageRepository; // final
+
+    @Transactional
     @Override
     public Long register(MovieDTO movieDTO) {
-        return null;
+
+        Map<String, Object> entityMap = dtoEntity(movieDTO);
+        Movie movie = (Movie) entityMap.get("movie");
+        List<MovieImage> movieImageList = (List<MovieImage>)entityMap.get("imgList");
+
+        movieRepository.save(movie);
+
+        movieImageList.forEach(movieImage -> {
+            imageRepository.save(movieImage);
+        });
+        return movie.getMno();
     }
 }
